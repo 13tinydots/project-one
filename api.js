e7885b2b938b57c56eba08a85847990a832f5ae4
Amanda Kays <amanda@Amandas-MacBook-Air.local>
2021-06-14 16:14:50 -0500

    fetch and input for weather api

--- api.js
+++ api.js
@@ -1,12 +1,12 @@
-// const BASE_URL = "https://jsonplaceholder.typicode.com/todos";
+const BASE_URL = "https://api.weatherbit.io/v2.0/forecast/daily?city=";
+const API_KEY = "&key=fb8d1c8508e9494f81c57432b5139296";
+let inputValue = document.querySelector(".inputValue");
 
-// export default {
-//   getTodos() {
-//     const todos = fetch(BASE_URL)
-//       .then((res) => res.json())
-//       .then((todos) => todos);
-//   },
-// };
-
-// //literally just adding this hear so ESLINT won't give me a commit error
-// getTodos();
+export default {
+  getWeather() {
+    return fetch(BASE_URL + inputValue + API_KEY)
+      .then((res) => res.json())
+      .then((weather) => weather);
+  },
+};
+inputValue = "chicago";
--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-// Main logic here//
+/* const button = document.querySelector(".button");
+const inputValue = document.querySelector(".input");
+
+button.addEventListener("click", function () {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      inputValue.value +
+      "&appid=dbc0e909c76459d6d2b6a5328a9579d5"
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}); */
+
+import api from "./api.js";
+let weather = null;
+
+api.getWeather().then((weatherData) => {
+  weather = weatherData;
+  console.log(weather);
+});
