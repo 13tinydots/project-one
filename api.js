efac012afed0053fce322ca4272fb92d29c995ba
13tinydots <13tinydots@users.noreply.github.com>
2021-06-16 22:18:25 -0500

    api click updated

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
-const weatherUrl = "https://api.weatherbit.io/v2.0/forecast/daily?city=";
+const weatherUrl = "https://api.weatherbit.io/v2.0/current?city=";
 const weatherKey = "&key=fb8d1c8508e9494f81c57432b5139296";
-const giphyUrl = "api.giphy.com/v1/gifs/search";
+const giphyUrl = "https://api.giphy.com/v1/gifs/search";
 const giphyApiKey = "api_key:uyn9unAjUTeMmkxYYnZiNGXLoGvh0rix";
 
 export default {
@@ -9,9 +9,10 @@ export default {
       .then((res) => res.json())
       .then((weather) => weather);
   },
-  getGif(gif) {
-    return fetch(giphyUrl + gif + giphyApiKey)
-      .then((res) => res.json())
-      .then((gif) => gif);
-  },
 };
+//   getGif(gif) {
+//     return fetch(giphyUrl + gif + giphyApiKey)
+//       .then((res) => res.json())
+//       .then((gif) => gif);
+//   },
+// };
--- scripts.js
+++ scripts.js
@@ -1,35 +1,21 @@
-/* const button = document.querySelector(".button");
-const inputValue = document.querySelector(".input");
-
-button.addEventListener("click", function () {
-  fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-      inputValue.value +
-      "&appid=dbc0e909c76459d6d2b6a5328a9579d5"
-  )
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-}); */
-
 import api from "./api.js";
 
 let weather = null;
-let gif = null;
-// TODO: Grab the form
-// TODO: Add an event listener for submission
-// TODO: Use the value from that input to pass to...
-document.querySelector("form").addEventListener("sumbit", (ev) => {
+const gif = null;
+
+document.querySelector("form").addEventListener("submit", (ev) => {
   ev.preventDefault();
 
   console.log(ev.target.elements[0].value);
 
-  api.getWeather().then((weatherData) => {
+  api.getWeather(ev.target.elements[0].value).then((weatherData) => {
     weather = weatherData;
     console.log(weather);
   });
 });
+
 /* giphy import */
-api.getGif().then((gifData) => {
-  gif = gifData;
-  console.log(gif);
-});
+// api.getGif().then((gifData) => {
+//   gif = gifData;
+//   console.log(gif);
+// });
