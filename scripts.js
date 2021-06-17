0559003737cef169a86a0d99b5f2a5ba2d68f66e
Amanda Kays <amanda@Amandas-MacBook-Air.local>
2021-06-17 10:51:17 -0500

    fixed temp ID

--- scripts.js
+++ scripts.js
@@ -5,17 +5,18 @@ const gif = null;
 const tempText = document.getElementById("conditionDesc");
 const windText = document.getElementById("windSpeed");
 const humText = document.getElementById("humidity");
-const condText = document.getElementById("conditionDesc");
+const condText = document.getElementById("temperature");
 
 document.querySelector("form").addEventListener("submit", (ev) => {
   ev.preventDefault();
   api.getWeather(ev.target.elements[0].value).then((weatherData) => {
     weather = weatherData;
-    const temp = weather.data[0].temp;
+    const temp = weather.data[0].app_temp;
     const wind = weather.data[0].wind_spd;
     const hum = weather.data[0].rh;
     const condition = weather.data[0].weather.description;
     console.log(temp);
+    console.log(weather);
 
     tempText.innerText = `${[temp]} degrees`;
     windText.innerText = `${[wind]} m/s`;
